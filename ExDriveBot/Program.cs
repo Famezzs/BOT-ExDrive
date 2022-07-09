5ba2ff908168e8a4cb11a2d9cd4d39cf37d7581c
Famezzs <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-07-09 16:20:09 +0300

    Started to implement docker support

--- ExDriveBot/Program.cs
+++ ExDriveBot/Program.cs
@@ -43,10 +43,12 @@ namespace ExDriveBot
         private static readonly string _auditName = "audit.json";
         private static readonly string _updatesName = "updates.json";
 
-        private static readonly HttpClientHandler _clientHandler = new HttpClientHandler();
-        private static System.Net.Http.HttpClient _http;
-        private static readonly TelegramBotClient _client = new(_token, _http, "http://localhost:8081/");
-        private static readonly string _postUrl = "https://localhost:443/Storage/UploadTempFileBot";
+        private static readonly HttpClientHandler _clientHandler = new();
+        private static HttpClient _http;
+        private static TelegramBotClient _client;
+        
+        private static string _postUrl = String.Empty;
+        private static string _apiUrl = String.Empty;
 
         private static List<Updates> _updatesCount = new();
         private static List<BotUpdate> _botUpdates = new();
@@ -57,8 +59,28 @@ namespace ExDriveBot
 
         private static long _chatid;
 
-        static void Main()
+        static void Main(string WEBPORT, string APISERVERPORT)
         {
+            if (String.IsNullOrEmpty(WEBPORT))
+            {
+                _postUrl = "https://localhost:443/Storage/UploadTempFileBot";
+            }
+            else
+            {
+                _postUrl = $"https://localhost:{WEBPORT}/Storage/UploadTempFileBot";
+            }
+
+            if (String.IsNullOrEmpty(APISERVERPORT))
+            {
+                _apiUrl = "http://localhost:8081/";
+            }
+            else
+            {
+                _apiUrl = $"http://localhost:{APISERVERPORT}/";
+            }
+
+            _client = new(_token, _http, _apiUrl);
+
             _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
             _http = new(_clientHandler);
 
@@ -89,7 +111,7 @@ namespace ExDriveBot
             var receiverOptions = new ReceiverOptions
             {
                 Offset = _updatesCount.ToArray()[0].UpdatesCount,
-                Limit = 100,
+
                 AllowedUpdates = new UpdateType[]
                 {
                     UpdateType.Message,
@@ -103,7 +125,7 @@ namespace ExDriveBot
 
         private static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
-            throw new Exception("An error occured");
+            throw new Exception($"An error occured: {arg2.Message}");
         }
 
         private static async void SendMessageAsync(string message, CancellationToken token)
@@ -131,12 +153,14 @@ namespace ExDriveBot
                     return;
                 }
 
-                if (update.Message.Type == MessageType.Voice)
+                if (update.Message.Type == MessageType.Audio ||
+                    update.Message.Type == MessageType.Photo ||
+                    update.Message.Type == MessageType.Video ||
+                    update.Message.Type == MessageType.Voice ||
+                    update.Message.Type == MessageType.Unknown)
                 {
-                    InputOnlineFile onlineFile = new("https://media.giphy.com/media/848sepFf5US1WT8L7N/giphy.gif");
-
-                    _ = await _client.SendAnimationAsync(update.Message.Chat.Id, onlineFile, cancellationToken: arg3);
-
+                    SendMessageAsync($@"Please send me files as documents instead.", arg3);
+                    
                     return;
                 }
 
@@ -145,24 +169,10 @@ namespace ExDriveBot
                     return;
                 }
 
-                if (update.Message.Type == MessageType.Audio ||
-                    update.Message.Type == MessageType.Photo ||
-                    update.Message.Type == MessageType.Video ||
-                    update.Message.Type == MessageType.Unknown)
-                {
-                    SendMessageAsync($@"Please send me files as documents instead.", arg3);
-                    //_ = await _client.SendTextMessageAsync(update.Message.Chat.Id,
-                    //    $@"Please send me files as documents instead.", cancellationToken: arg3);
-
-                    return;
-                }
-
                 if (update.Message.Document.FileSize > _maxFileSize)
                 {
                     SendMessageAsync($@"File size should be more than 650mb.", arg3);
-                    //_ = await _client.SendTextMessageAsync(update.Message.Chat.Id,
-                    //    $@"File size should be more than 650mb.", cancellationToken: arg3);
-
+                    
                     return;
                 }
 
@@ -170,9 +180,7 @@ namespace ExDriveBot
                 string name = update.Message.Document.FileName;
 
                 SendMessageAsync($"Download for \"{name}\" started... You will be notified shortly once the link is ready!", arg3);
-                //_ = await _client.SendTextMessageAsync(_chatid, 
-                //    $"Started upload for {name}. You will be notified once the link is ready!", cancellationToken: arg3);
-
+                
                 Telegram.Bot.Types.File _download;
                 _download = await _client.GetFileAsync(id, cancellationToken: arg3);
 
@@ -202,10 +210,7 @@ namespace ExDriveBot
                     {
                         SendMessageAsync($"Download of \"{name}\" failed. Please try again.",
                             arg3);
-                        //_ = await _client.SendTextMessageAsync(_chatid, 
-                        //    $"Download of \"{name}\" failed. Please try again.",
-                        //    cancellationToken: arg3);
-
+                        
                         return;
                     }
                 }
@@ -231,8 +236,6 @@ namespace ExDriveBot
 
                 _botUpdates.Add(botUpdate);
 
-                //Console.WriteLine($"\"{name.Unidecode()}\" finished downloading ({size} bits)");
-
                 var botUpdatesString = JsonConvert.SerializeObject(_botUpdates);
                 var updatesNumberString = JsonConvert.SerializeObject(_updatesCount);
 
@@ -242,16 +245,11 @@ namespace ExDriveBot
                 if (!string.IsNullOrEmpty(downloadlink))
                 {
                     SendMessageAsync($"Download link for \"{name}\": {downloadlink}", arg3);
-                    //_ = await _client.SendTextMessageAsync(_chatid, $"Download link for \"{name}\": " + downloadlink,
-                    //    cancellationToken: arg3);
                 }
                 else
                 {
                     SendMessageAsync($@"Oops... We are sorry, but it looks like something went wrong. Please try again.",
                         arg3);
-                    //_ = await _client.SendTextMessageAsync(_chatid,
-                    //    $@"Oops... We are sorry, but it looks like something went wrong. Please try again.",
-                    //    cancellationToken: arg3);
                 }
 
                 Directory.Delete(Path.Combine(_absPath, _newName), true);
@@ -260,10 +258,7 @@ namespace ExDriveBot
             {
                 SendMessageAsync($@"Oops... We are sorry, but it looks like something went wrong. Please try again.",
                         arg3);
-                //_ = await _client.SendTextMessageAsync(_chatid, 
-                //    $@"Oops... We are sorry, but it looks like something went wrong. Please try again.",
-                //    cancellationToken: arg3);
-
+                
                 Directory.Delete(Path.Combine(_absPath, _newName), true);
 
                 return;
